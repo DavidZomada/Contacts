af0cc335c656f610eb7f35dc50c91632af6b16c8
DavidZomada <100346027@alumnos.uc3m.es>
2022-01-18 00:00:27 +0100

    primer commit

---
+++ Contacts/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using Contacts.ViewModels;
+
+namespace Contacts.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
